d9932a01ddc239cc3a86061f88f0c4386ad8bdd4
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2024-03-20 00:07:04 -0500

    feat: archivos de configuracion
    Se agregaron colores y medidas al archivo de configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,18 @@ export default {
     extend: {
       colors: {
         yellow: { 
-          "primary": "#EED023"
+          "primary": "#EED023",
+          "secondary": "#FFC107"
         },
         purple: {
           "modal": "#0F0122",
           "oscure": "#070010"
-        }
+        },
+      },
+      screens: {
+        "xs": "350px",
+        "mdx": "1149px",
+        "3xl": "1730px"
       }
     },
   },
