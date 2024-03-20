46b0748b2015d8224b0836176c06b1264763d111
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2024-03-20 00:16:20 -0500

    feat: creacion de tipos
    tipados para funcionalidades
    
    - buttonCTATYPES
    - cardOwnerTypes

--- src/types/CardOwner.types.ts
+++ src/types/CardOwner.types.ts
@@ -6,6 +6,4 @@ type CardOwnerSubheadline = {
   end: string;
 };
 
-
-
 export { CardOwnerDescription, CardOwnerSubheadline}
--- src/types/buttonCTA.types.ts
+++ src/types/buttonCTA.types.ts
@@ -2,6 +2,7 @@ type buttonCTATypeStyles = {
   variant: "cta" | "success" | "danger",
   width?: string,
   heigth?: string
+  fontSize?: string 
 };
 
 type buttonCTATypeText = string;
